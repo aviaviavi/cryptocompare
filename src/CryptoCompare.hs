d91f298af63472c7a01b541fb258dbdc9b13fe2c
aviaviavi <avipress@gmail.com>
2017-09-22 20:54:26 -0700

    more docs

--- src/CryptoCompare.hs
+++ src/CryptoCompare.hs
@@ -3,6 +3,23 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 -- | A haskell wrapper for the cryptocompare API, a source of information and pricing of different crypto currencies
+--
+-- > module Main (main) where
+-- >
+-- > import CryptoCompare
+-- >
+-- > main :: IO ()
+-- > main = do
+-- >   coinList <- fetchCoinList
+-- >   either print (print . length) coinList
+-- >   either print (print . head) coinList
+-- >   priceResp <- fetchCurrentPrice "BTC" ["USD", "EUR", "BTC"]
+-- >   print priceResp
+-- >   priceHistResp <- fetchDailyPriceHistory "BTC" "USD" 300
+-- >   print priceHistResp
+-- >   snapshotResp <- fetchCoinSnapshot "BTC" "USD"
+-- >   print snapshotResp
+--
 module CryptoCompare
   ( fetchCoinList
   , fetchCurrentPrice
